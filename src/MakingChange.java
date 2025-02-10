532a468e3c9252246bef48c5ee7c798e154454a1
Echandler123 <elijah.chandler@menloschool.org>
2025-02-09 23:22:30 -0800

    update

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -15,20 +15,36 @@ public class MakingChange {
      */
     public static long countWays(int target, int[] coins) {
         Arrays.sort(coins);
-        // create adjacency list for each way/path for to make each target
-        ArrayList<Integer> ways = new ArrayList<Integer>();
-        if(target < 0){
-            return 0;
+        // Create a 2D array to store the number of ways to make change for each target
+        long[][] ways = new long[target + 1][coins.length];
+        // Initialize the ways array
+        for (int i = 0; i < coins.length; i++) {
+                ways[0][i] = 1;
         }
-        return 0;
+        return count(target, coins, ways, 0);
     }
-    public static long findways(ArrayList<Integer> ways, int target, int[] coins){
-        // if the target is 0, then there is only one way to make change
-        if(target == 1 && coins[0] == 1){
+    public static long count(int target, int[] coins, long[][] ways, int index) {
+        // If the target is 0, then there is only one way to make change
+        if(target == 0) {
             return 1;
         }
-        // if the target is less than 0, then there is no way to make change
-
-        return 0;
+        // If the target is less than 0, then there is no way to make change
+        if(target < 0 || index >= coins.length) {
+            return 0;
+        }
+        // If there are no coins, then there is no way to make change
+        if(coins.length == 0) {
+            return 0;
+        }
+        // If the number of ways to make change for the target and index has been found return it
+        if(ways[target][index] != 0) {
+            return ways[target][index];
+        }
+        // Count the number of ways to make change for the target using the current coin
+        long includeCurrent = count(target - coins[index], coins, ways, index);
+        // Count the number of ways to make change for the target without using the current coin
+        long excludeCurrent = count(target, coins, ways, index + 1);
+        ways[target][index] =  includeCurrent + excludeCurrent;
+        return ways[target][index];
     }
 }
