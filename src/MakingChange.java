85d7711fd5f858159fcf0cd5b3a7533126e6230e
Echandler123 <elijah.chandler@menloschool.org>
2025-02-10 00:04:28 -0800

    update

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -23,28 +23,48 @@ public class MakingChange {
         }
         return count(target, coins, ways, 0);
     }
+    // Memoization approach
+//    public static long count(int target, int[] coins, long[][] ways, int index) {
+//        // If the target is 0, then there is only one way to make change
+//        if(target == 0) {
+//            return 1;
+//        }
+//        // If the target is less than 0, then there is no way to make change
+//        if(target < 0 || index >= coins.length) {
+//            return 0;
+//        }
+//        // If there are no coins, then there is no way to make change
+//        if(coins.length == 0) {
+//            return 0;
+//        }
+//        // If the number of ways to make change for the target and index has been found return it
+//        if(ways[target][index] != 0) {
+//            return ways[target][index];
+//        }
+//        // Count the number of ways to make change for the target using the current coin
+//        long includeCurrent = count(target - coins[index], coins, ways, index);
+//        // Count the number of ways to make change for the target without using the current coin
+//        long excludeCurrent = count(target, coins, ways, index + 1);
+//        ways[target][index] =  includeCurrent + excludeCurrent;
+//        return ways[target][index];
+//    }
+    // Tabulation approach
     public static long count(int target, int[] coins, long[][] ways, int index) {
-        // If the target is 0, then there is only one way to make change
-        if(target == 0) {
-            return 1;
+        // Go through each coin starting with the first
+        for (int i = 0; i < coins.length; i++) {
+            // Starts the target at 1 and goes until the actual target value
+            for (int j = 1; j <= target; j++) {
+                // If the current value for target is greater than or equal to the current coin value, then there is
+                // a way to make change
+                if (j - coins[i] >= 0) {
+                    ways[j][i] += ways[j - coins[i]][i];
+                }
+                // If this is not the very first coin, then add the previous ways to make change for the target
+                if (i > 0) {
+                    ways[j][i] += ways[j][i - 1];
+                }
+            }
         }
-        // If the target is less than 0, then there is no way to make change
-        if(target < 0 || index >= coins.length) {
-            return 0;
-        }
-        // If there are no coins, then there is no way to make change
-        if(coins.length == 0) {
-            return 0;
-        }
-        // If the number of ways to make change for the target and index has been found return it
-        if(ways[target][index] != 0) {
-            return ways[target][index];
-        }
-        // Count the number of ways to make change for the target using the current coin
-        long includeCurrent = count(target - coins[index], coins, ways, index);
-        // Count the number of ways to make change for the target without using the current coin
-        long excludeCurrent = count(target, coins, ways, index + 1);
-        ways[target][index] =  includeCurrent + excludeCurrent;
-        return ways[target][index];
+        return ways[target][coins.length - 1];
     }
 }
